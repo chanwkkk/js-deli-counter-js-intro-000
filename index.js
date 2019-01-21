69c1caf953a7ae34e5643beefaa1b43d6ed8b22d
Yingqi Chen <chanwk18@hotmail.com>
2019-01-21 08:03:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,23 @@ function takeANumber(array,name){
 function nowServing(array){
   if (array.length===0){
     return There is nobody waiting to be served!
+
   }else{
     var name=array[0];
     array.shift();
-    return `Currently serving ${name}` ;
+    return `Currently serving ${name}.` ;
+  }
+}
+
+function currentLine(array){
+  if(array.length===0){
+    return The line is currently empty.
+  }else{
+    function order(){
+      for(var i=0;i<array.length;i++){
+      return `${i+1}:${array[i]}`
+    }
+  return `The line is currently: ${function order()}`
+
   }
 }
